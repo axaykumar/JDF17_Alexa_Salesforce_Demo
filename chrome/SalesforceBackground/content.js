290d55f08473466677c882cec439c9dc85312c78
axaykumar <axay.varu@gmail.com>
2017-03-26 14:36:15 +0530

    Delete content.js

--- chrome/SalesforceBackground/content.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    url = chrome.extension.getBackgroundPage().url;
-    host = url.split('.com/')[0];
-    host = host + '.com';
-    window.location.href = host + '/apex/SalesforceBackgroundControl';
-}
-
